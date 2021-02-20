0c248d65795b7715da5833bd3dab8b7a160e8661
Niklas <niklas@long.ch>
2021-02-20 08:51:49 +0100

    ref: make generic over Id

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,66 @@
 // 3. Graph is the main returned data structure. Diff could be envisaged for D3. Custom nannou impl
 //    might not need it.
 
-use pea2pea::{connect_nodes, Node, NodeConfig, Pea2Pea, Topology};
+use core::fmt::Debug;
+use pea2pea::{Node, Pea2Pea};
+use std::hash::Hash;
 use std::{collections::HashSet, net::SocketAddr};
 
-const N: usize = 5;
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Vertex<T> {
+    id: T,
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Edge<T> {
+    source: T,
+    target: T,
+}
+
+#[derive(Debug)]
+struct Graph<T> {
+    vertices: HashSet<Vertex<T>>,
+    edges: HashSet<Edge<T>>,
+}
+
+trait AsGraph {
+    type Id;
+
+    fn id(&self) -> Self::Id;
+
+    fn active_connections(&self) -> Vec<Self::Id>;
+
+    fn graph(nodes: &[Self]) -> Graph<Self::Id>
+    where
+        Self: Sized,
+        <Self as AsGraph>::Id: Eq + Hash + Copy,
+    {
+        let mut vertices = HashSet::new();
+        let mut edges = HashSet::new();
+
+        // Used only for dedup purposes.
+        let mut connected_pairs = HashSet::new();
+
+        for node in nodes {
+            let own_addr = node.id();
+            vertices.insert(Vertex { id: own_addr });
+
+            for addr in node.active_connections() {
+                if own_addr != addr
+                    && connected_pairs.insert((own_addr, addr))
+                    && connected_pairs.insert((addr, own_addr))
+                {
+                    edges.insert(Edge {
+                        source: own_addr,
+                        target: addr,
+                    });
+                }
+            }
+        }
+
+        Graph { vertices, edges }
+    }
+}
 
 struct JustANode(pub Node);
 
@@ -30,57 +86,25 @@ impl std::ops::Deref for JustANode {
     }
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
-struct Vertex {
-    id: SocketAddr,
-}
+impl AsGraph for JustANode {
+    type Id = SocketAddr;
 
-#[derive(Hash, Eq, PartialEq, Debug)]
-struct Edge {
-    source: SocketAddr,
-    target: SocketAddr,
-}
+    fn id(&self) -> Self::Id {
+        self.listening_addr()
+    }
 
-#[derive(Debug)]
-struct Graph {
-    vertices: HashSet<Vertex>,
-    edges: HashSet<Edge>,
-}
-
-impl Graph {
-    fn from(nodes: &[JustANode]) -> Self {
-        let mut vertices = HashSet::new();
-        let mut edges = HashSet::new();
-
-        // Used only for dedup purposes.
-        let mut connected_pairs = HashSet::new();
-
-        for node in nodes {
-            let own_addr = node.listening_addr();
-            vertices.insert(Vertex { id: own_addr });
-
-            let peers_guard = node.known_peers().read();
-
-            for (addr, _) in peers_guard.iter() {
-                if own_addr != *addr
-                    && connected_pairs.insert((own_addr, *addr))
-                    && connected_pairs.insert((*addr, own_addr))
-                {
-                    edges.insert(Edge {
-                        source: own_addr,
-                        target: *addr,
-                    });
-                }
-            }
-        }
-
-        Self { vertices, edges }
+    fn active_connections(&self) -> Vec<Self::Id> {
+        self.known_peers().read().keys().copied().collect()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use pea2pea::{connect_nodes, NodeConfig, Topology};
+
+    const N: usize = 5;
 
     #[tokio::test]
     async fn it_works() {
@@ -100,7 +124,7 @@ mod tests {
 
         let nodes: Vec<JustANode> = nodes.into_iter().map(JustANode).collect();
 
-        connect_nodes(&nodes, Topology::Ring).await;
-        dbg!(Graph::from(&nodes));
+        connect_nodes(&nodes, Topology::Ring).await.unwrap();
+        dbg!(JustANode::graph(&nodes));
     }
 }
